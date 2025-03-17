fc8e83cf949d86fc1149a376b68714278f36102e
[jesus romero] <jesus.romerovidal@gmail.com>
2025-03-17 20:11:31 +0100

    enhance FractionTest #9

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -1,6 +1,8 @@
 package es.upm.miw.iwvg_devops.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,6 +32,36 @@ public class FractionTest {
     @Test
     void testDecimal() {
         assertEquals(0.5, fraction.decimal());
+    }
+
+    @Test
+    void testIsProper() {
+        assertTrue(fraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertFalse(fraction.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        assertTrue(fraction.isEquivalent(new Fraction(4, 8)));
+        assertFalse(fraction.isEquivalent(new Fraction(1, 3)));
+    }
+
+    @Test
+    void testDivide() {
+        fraction = fraction.divide(new Fraction(2, 4));
+        assertEquals(8, fraction.getNumerator());
+        assertEquals(8, fraction.getDenominator());
+    }
+
+    @Test
+    void testMultiply() {
+        fraction = fraction.multiplay(new Fraction(2, 4));
+        assertEquals(4, fraction.getNumerator());
+        assertEquals(16, fraction.getDenominator());
     }
 
 }
