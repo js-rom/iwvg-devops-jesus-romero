1e99ba6b2acf581470c5150514aa97a23db81df7
[jesus romero] <jesus.romerovidal@gmail.com>
2025-03-18 13:36:06 +0100

    add search 3 #16

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -7,43 +7,51 @@ import java.util.stream.Stream;
 @Service
 public class Searches {
 
-    public Searches() {
+        public Searches() {
 
-    }
+        }
 
-    public Stream<String> findUserFamilyNameByUserNameDistinct(String userName) {
-        return new UsersDatabase().findAll()
-                .filter(user -> userName.equals(user.getName()))
-                .map(User::getFamilyName)
-                .distinct();
-    }
+        public Stream<String> findUserFamilyNameByUserNameDistinct(String userName) {
+                return new UsersDatabase().findAll()
+                                .filter(user -> userName.equals(user.getName()))
+                                .map(User::getFamilyName)
+                                .distinct();
+        }
 
-    public Stream<Integer> findFractionNumeratorByUserFamilyName(String userFamilyName) {
-        return new UsersDatabase().findAll()
-                .filter(user -> userFamilyName.equals(user.getFamilyName()))
-                .flatMap(user -> user.getFractions().stream())
-                .map(Fraction::getNumerator);
-    }
+        public Stream<Integer> findFractionNumeratorByUserFamilyName(String userFamilyName) {
+                return new UsersDatabase().findAll()
+                                .filter(user -> userFamilyName.equals(user.getFamilyName()))
+                                .flatMap(user -> user.getFractions().stream())
+                                .map(Fraction::getNumerator);
+        }
 
-    public Stream<String> findUserFamilyNameByFractionDenominator(int fractionDenominator) {
-        return new UsersDatabase().findAll()
-                .filter(user -> user.getFractions().stream()
-                        .anyMatch(fraction -> fractionDenominator == fraction.getDenominator()))
-                .map(User::getFamilyName);
-    }
+        public Stream<String> findUserFamilyNameByFractionDenominator(int fractionDenominator) {
+                return new UsersDatabase().findAll()
+                                .filter(user -> user.getFractions().stream()
+                                                .anyMatch(fraction -> fractionDenominator == fraction.getDenominator()))
+                                .map(User::getFamilyName);
+        }
 
-    public Stream<String> findUserFamilyNameInitialByAnyProperFraction() {
-        return new UsersDatabase().findAll()
-                .filter(user -> user.getFractions().stream()
-                        .anyMatch(Fraction::isProper))
-                .map(User::initials);
-    }
+        public Stream<String> findUserFamilyNameInitialByAnyProperFraction() {
+                return new UsersDatabase().findAll()
+                                .filter(user -> user.getFractions().stream()
+                                                .anyMatch(Fraction::isProper))
+                                .map(User::initials);
+        }
 
-    public Stream<String> findUserIdByAnyProperFraction() {
-        return new UsersDatabase().findAll()
-                .filter(user -> user.getFractions().stream()
-                        .anyMatch(Fraction::isProper))
-                .map(User::getId);
-    }
+        public Stream<String> findUserIdByAnyProperFraction() {
+                return new UsersDatabase().findAll()
+                                .filter(user -> user.getFractions().stream()
+                                                .anyMatch(Fraction::isProper))
+                                .map(User::getId);
+        }
+
+        public Fraction findFractionMultiplicationByUserFamilyName(String familyName) {
+                return new UsersDatabase().findAll()
+                                .filter(user -> user.getFamilyName().equals(familyName))
+                                .flatMap(user -> user.getFractions().stream())
+                                .reduce(new Fraction(1, 1), (subtotal, item) -> Fraction.multiplay(subtotal, item));
+
+        }
 
 }
